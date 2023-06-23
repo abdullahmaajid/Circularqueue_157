c17d3fe697a9ff6cc6b018686bf542b1a0be94bb
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-06-23 11:21:53 +0700

    done

--- Circularqueue/Circularqueue.cpp
+++ Circularqueue/Circularqueue.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 class Queues {
-	int FRONT, REAR, max = 5;
+	int FRONT, REAR, n = 5;
 	int queue_array[5];
 
 public:
@@ -18,11 +18,11 @@ public:
 		cout << endl;
 
 		// cek apakah antrian penuh
-		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+		if ((FRONT == 0 && REAR == n - 1) || (FRONT == REAR + 1)) {
 			cout << "\n Queue overflow \n";
 			return;
 		}
-	
+
 		// cek apakah antrian kosong
 		if (FRONT == -1) {
 			FRONT = 0;
@@ -30,7 +30,7 @@ public:
 		}
 		else {
 			// Jika REAR berada di posisi terakhir array, kembali ke awal array
-			if (REAR == max - 1)
+			if (REAR == n - 1)
 				REAR = 0;
 			else
 				REAR = REAR + 1;
@@ -53,12 +53,93 @@ public:
 		}
 		else {
 			// jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
-			if (FRONT == max - 1)
+			if (FRONT == n - 1)
 				FRONT = 0;
 			else
 				FRONT = FRONT - 1;
 		}
 	}
 
+	void display() {
+		int FRONT_position = FRONT;
+		int REAR_position = REAR;
+
+		// CEK APAKAH ANTRIAN KOSONG
+		if(FRONT == -1) {
+			cout << "QUEUE IS EMPTY \N";
+			return;
+		}
+
+		cout << "\N element in the queue are...\n";
+
+		//jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+		else {
+			//jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+			while (FRONT_position <= n - 1) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+
+			FRONT_position = 0;
+
+			//iterasi dari awal array hingga akhir REAR
+			while (FRONT_position << REAR_position) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+	}
 
 };
+
+int main() {
+	Queues q;
+	char ch;
+
+	while (true) {
+		try {
+			cout << "MENU" << endl;
+			cout << "1. Implement insert opertaions" << endl;
+			cout << "2. Implement delete opertaions" << endl;
+			cout << "3. Display Values" << endl;
+			cout << "4. Exit" << endl;
+			cout << "Enter your choice (1-4): ";
+			cin >> ch;
+			cout << endl;
+
+			switch (ch) {
+			case '1': {
+
+				q.insert();
+				break;
+			}
+			case '2': {
+
+				q.remove();
+				break;
+			}
+			case '3': {
+				q.display();
+				break;
+			}
+			case '4': {
+				cout << "Invalid option!" << endl;
+				break;
+			}
+			}
+		}
+		catch (exception& e) {
+			cout << "Check for the values entered." << endl;
+		}
+	}
+
+	return 0;
+}
