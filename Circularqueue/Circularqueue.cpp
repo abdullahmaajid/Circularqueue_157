af2b110b78952c248e0ee6d85e1c666fa6848b7b
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-06-23 09:39:44 +0700

    cek jika antrian hanya memiliki satu element & jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array

--- Circularqueue/Circularqueue.cpp
+++ Circularqueue/Circularqueue.cpp
@@ -41,7 +41,22 @@ public:
 	void remove() {
 		// cek apakah ada antrian kosong
 		if (FRONT == -1) {
+			cout << "Queue underflow \n";
+			return;
+		}
+		cout << "\n The element deleted from the queue is : " << queue_array[FRONT] << "\n";
 
+		// cek jika antrian hanya memiliki satu element
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
+		else {
+			// jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT - 1;
 		}
 	}
 
